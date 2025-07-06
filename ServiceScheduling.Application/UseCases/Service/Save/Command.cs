6f87f0d31245cf9d3b7b6b9062ee9b954f23be5d
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-05 20:44:43 -0400

    REFACTOR: Adjusting UseCases/Service/Save to receive CreateServiceWithFileDto and convert it to CreateServiceDto

--- ServiceScheduling.Application/UseCases/Service/Save/Command.cs
+++ ServiceScheduling.Application/UseCases/Service/Save/Command.cs
@@ -4,4 +4,4 @@ using ServiceScheduling.Domain.Interfaces;
 
 namespace ServiceScheduling.Application.UseCases.Service.Save;
 
-public sealed record Command(CreateServiceDto Service) : IRequest<Result<Response>>;
+public sealed record Command(CreateServiceWithFileDto Service, string? ImagePath) : IRequest<Result<Response>>;
--- ServiceScheduling.Application/UseCases/Service/Save/Handler.cs
+++ ServiceScheduling.Application/UseCases/Service/Save/Handler.cs
@@ -1,10 +1,12 @@
 using MediatR;
+using ServiceScheduling.Application.DTOs.Service;
 using ServiceScheduling.Application.Extensions;
 using ServiceScheduling.Domain.Interfaces;
 
 namespace ServiceScheduling.Application.UseCases.Service.Save;
 
-public class Handler(IServiceRepository serviceRepository, IUserRepository userRepository) : IRequestHandler<Command, Result<Response>>
+public class Handler(IServiceRepository serviceRepository, IUserRepository userRepository)
+    : IRequestHandler<Command, Result<Response>>
 {
     public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
     {
@@ -15,24 +17,33 @@ public class Handler(IServiceRepository serviceRepository, IUserRepository userR
             var value = prop.GetValue(request.Service);
 
             if (value == null) return Result.Failure<Response>(new Error("400", $"Argument {prop.Name} is null"));
-            
+
             if (prop.PropertyType == typeof(string) && string.IsNullOrWhiteSpace(value.ToString()))
                 return Result.Failure<Response>(new Error("400", $"Argument {prop.Name} is null"));
         }
-        
-        var verifyUserRole = await userRepository.GetByIdAsync(request.Service.ProviderId);
 
-        if (verifyUserRole != null && verifyUserRole.ProfileId == 2)
+        var verifyUserRole = await userRepository.GetByIdAsync(request.Service.ProviderId, cancellationToken);
+
+        if (verifyUserRole?.ProfileId == 2)
         {
-            var entity = request.Service.ToEntity();
+            CreateServiceDto service;
+
+            if (request.ImagePath == null)
+            {
+                service = request.Service.ToCreateServiceDto(null);
+            }
+            else
+            {
+                service = request.Service.ToCreateServiceDto(request.ImagePath);
+            }
+
+            var entity = service.ToEntity();
             await serviceRepository.SaveAsync(entity, cancellationToken);
         }
         else
         {
             return Result.Failure<Response>(new Error("401", "you are not authorized"));
         }
-        
-        //TODO: Salvar a foto na AWS S3.
 
         return verifyUserRole is null
             ? Result.Failure<Response>(new Error("400", $"Provider is null"))
