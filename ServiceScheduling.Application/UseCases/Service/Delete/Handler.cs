d3ea637d8face1a9ca8e34f7de562191c88f5679
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-08 13:40:38 -0400

    REFACTOR: Deleting image when deleting a service

--- ServiceScheduling.Application/UseCases/Service/Delete/Handler.cs
+++ ServiceScheduling.Application/UseCases/Service/Delete/Handler.cs
@@ -1,16 +1,23 @@
 using MediatR;
+using ServiceScheduling.Application.Interfaces;
 using ServiceScheduling.Domain.Interfaces;
 
 namespace ServiceScheduling.Application.UseCases.Service.Delete;
 
-public sealed class Handler(IServiceRepository repository) : IRequestHandler<Command, Result<Response>>
+public sealed class Handler(IServiceRepository repository, IAwsS3Service s3Service)
+    : IRequestHandler<Command, Result<Response>>
 {
     public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
     {
         var serviceExists = await repository.GetByIdAsync(request.serviceId, cancellationToken);
-        
-        if(serviceExists == null) return Result.Failure<Response>(new Error("404", "User not found"));
-        
+
+        if (serviceExists == null) return Result.Failure<Response>(new Error("404", "User not found"));
+
+        if (!string.IsNullOrWhiteSpace(serviceExists.ImageUrl))
+        {
+            await s3Service.DeleteFileAsync(serviceExists.ImageUrl, cancellationToken);
+        }
+
         await repository.DeleteAsync(serviceExists, cancellationToken);
         return Result.Success(new Response());
     }
