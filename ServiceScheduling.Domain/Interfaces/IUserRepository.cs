f2c9a74cdcb70834d8c438fb9ba9e943646acd6a
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-10 16:11:38 -0400

    REFACTOR: Restricts UpdateAsync to not accept null

--- ServiceScheduling.Domain/Interfaces/IUserRepository.cs
+++ ServiceScheduling.Domain/Interfaces/IUserRepository.cs
@@ -7,6 +7,6 @@ public interface IUserRepository
     Task SaveAsync(User user, CancellationToken cancellationToken = default);
     Task<List<User>?> GetAllAsync(int skip = 0, int take = 10, CancellationToken cancellationToken = default);
     Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task UpdateAsync(User? user, CancellationToken cancellationToken = default);
+    Task UpdateAsync(User user, CancellationToken cancellationToken = default);
     Task DeleteAsync(User user, CancellationToken cancellationToken = default);
 }
