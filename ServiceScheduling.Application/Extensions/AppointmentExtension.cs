993a00a045793843db8d33e957eb6d9bec2a9e2d
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-10 16:37:36 -0400

    FEAT: Creating method toDto in AppointmentExtension.cs

--- ServiceScheduling.Application/Extensions/AppointmentExtension.cs
+++ ServiceScheduling.Application/Extensions/AppointmentExtension.cs
@@ -1,4 +1,6 @@
 using ServiceScheduling.Application.DTOs.Appointment;
+using ServiceScheduling.Application.DTOs.Service;
+using ServiceScheduling.Application.DTOs.User;
 using ServiceScheduling.Domain.Entities;
 
 namespace ServiceScheduling.Application.Extensions;
@@ -8,5 +10,16 @@ public static class AppointmentExtension
     public static Appointment ToEntity(this CreateAppointmentDto dto)
     {
         return new Appointment(dto.Date, dto.Status, dto.ClientId, dto.ServiceId);
+    }
+    
+    public static ViewAppointmentDto ToDto(this Appointment appointment)
+    {
+        return new ViewAppointmentDto
+        {
+            Date = appointment.Date,
+            Status = appointment.Status,  
+            Client = new ViewUserDto(appointment.Client.Name, appointment.Client.Email),
+            Service = new ViewServiceDto(appointment.Service.Name, appointment.Service.Description, appointment.Service.Price, appointment.Service.ImageUrl)
+        };
     }
 }
