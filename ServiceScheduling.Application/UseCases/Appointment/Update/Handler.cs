1b8ddab4d7bbe3bdccb347d009904dd59fbf41b0
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-11 14:42:57 -0400

    REFACTOR: Adding service not found and user not found check

--- ServiceScheduling.Application/UseCases/Appointment/Update/Handler.cs
+++ ServiceScheduling.Application/UseCases/Appointment/Update/Handler.cs
@@ -3,7 +3,7 @@ using ServiceScheduling.Domain.Interfaces;
 
 namespace ServiceScheduling.Application.UseCases.Appointment.Update;
 
-public class Handler(IAppointmentRepository repository) : IRequestHandler<Command, Result<Response>>
+public class Handler(IAppointmentRepository repository, IServiceRepository serviceRepository, IUserRepository userRepository) : IRequestHandler<Command, Result<Response>>
 {
     public async Task<Result<Response>> Handle(Command request, CancellationToken cancellationToken)
     {
@@ -18,8 +18,24 @@ public class Handler(IAppointmentRepository repository) : IRequestHandler<Comman
         
         if(appointmentData.Date.HasValue) appointment.UpdateDate(appointmentData.Date.Value);
         if(appointmentData.Status.HasValue) appointment.UpdateStatus(appointmentData.Status.Value);
-        if(appointmentData.ServiceId.HasValue) appointment.UpdateServiceId(appointmentData.ServiceId.Value);
-        if(appointmentData.ClientId.HasValue) appointment.UpdateClientId(appointmentData.ClientId.Value);
+        if (appointmentData.ServiceId.HasValue)
+        {
+            var serviceExists = await serviceRepository.GetByIdAsync(appointmentData.ServiceId.Value, cancellationToken); 
+            
+            if(serviceExists is null)
+                return Result.Failure<Response>(new Error("404", "Service not found"));
+            
+            appointment.UpdateServiceId(appointmentData.ServiceId.Value);
+        }
+        if(appointmentData.ClientId.HasValue)
+        {
+            var userExists = await userRepository.GetByIdAsync(appointmentData.ClientId.Value, cancellationToken); 
+            
+            if(userExists is null)
+                return Result.Failure<Response>(new Error("404", "User not found"));
+            
+            appointment.UpdateClientId(appointmentData.ClientId.Value);
+        }
         
         await repository.UpdateAsync(appointment, cancellationToken);
 
